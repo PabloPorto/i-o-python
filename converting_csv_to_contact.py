be780b1207109d1e605c01c034ab4a6794a14e18
pablo.assis <tr_passis@boavistascpc.com.br>
2021-09-09 03:48:16 -0300

    json_to_contacts

--- converting_csv_to_contact.py
+++ converting_csv_to_contact.py
@@ -1,10 +1,10 @@
-import csv
+import csv, pickle, json
 from contato import Contato
 
-def csv_to_contact(caminho, encoding='latin_1'):
+def csv_to_contact(path, encoding='latin_1'):
     contacts = []
 
-    with open(caminho,encoding=encoding) as file:
+    with open(path,encoding=encoding) as file:
         reader = csv.reader(file)
 
         for line in reader:
@@ -15,4 +15,37 @@ def csv_to_contact(caminho, encoding='latin_1'):
             contacts.append(contact)
 
 
+    return contacts
+
+def contacts_to_pickle(contacts, path):
+
+    with open(path, mode='wb') as file:
+        pickle.dump(contacts, file)
+
+def pickle_to_contacts(path):
+
+    with open(path, mode='rb') as file:
+        contacts =  pickle.load(file)
+
+    return contacts
+
+def contacts_to_json(contacts, path):
+    with open(path, mode='w') as file:
+        json.dump(contacts, file, default=_contact_to_json)
+
+def _contact_to_json(contact):
+    return contact.__dict__
+
+def json_to_contacts (path):
+    contacts = []
+
+    with open(path) as file:
+        contacts_json = json.load(file)
+
+        for contact in contacts_json:
+            c = Contato(id = contact['id'], nome = contact['nome'], email = contact['email'] )
+            # it could be like (unpacking):
+            # c = Contato(**contact)
+            contacts.append(c)
+    
     return contacts
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import converting_csv_to_contact
 
 try:
-    contacts = converting_csv_to_contact.csv_to_contact('dados/contatos.csv')
+    # contacts = converting_csv_to_contact.csv_to_contact('dados/contatos.csv')
+    # converting_csv_to_contact.contacts_to_pickle(contacts, 'dados/serialized_contacts.p')
+
+    # contacts = converting_csv_to_contact.pickle_to_contacts('dados/serialized_contacts.p')
+
+    # converting_csv_to_contact.contacts_to_json(contacts, 'dados/contacts_as_json.json')
+
+    contacts = converting_csv_to_contact.json_to_contacts('dados/contacts_as_json.json')
 
     for contact in contacts:
         print(f'{contact.id} - {contact.nome} - {contact.email}')
